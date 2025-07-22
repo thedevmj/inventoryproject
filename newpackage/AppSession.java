b36abb176f81720c432cd548429faa122b8fade0
Junaid mansoori <junaidmansuri71@gmail.com>
2025-07-22 10:51:38 +0530

    Add files via upload
    added completed classes to notes app

---
+++ newpackage/AppSession.java
@@ -0,0 +1,15 @@
+package newpackage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Junaid Mansuri
+ */
+public class AppSession {
+    public static int userid=-1;
+}
