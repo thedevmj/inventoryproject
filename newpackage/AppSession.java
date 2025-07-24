2e0c92c12aff45adfb204973e0070d8c0718abda
Junaid mansoori <junaidmansuri71@gmail.com>
2025-07-24 11:13:04 +0530

    Update AppSession.java

--- newpackage/AppSession.java
+++ newpackage/AppSession.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package newpackage;
 
 /*
@@ -14,20 +14,3 @@ package newpackage;
 public class AppSession {
     public static int userid=-1;
 }
-=======
-package newpackage;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Junaid Mansuri
- */
-public class AppSession {
-    public static int userid=-1;
-}
->>>>>>> b36abb176f81720c432cd548429faa122b8fade0
